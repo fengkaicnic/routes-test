7016761562d1666bdcd15794d3694a8021eb02af
fengkaicnic <fengkai@cnic.cn>
2014-12-18 16:09:45 +0800

    add a server

---
+++ routes-test/src/service/appserver.py
@@ -0,0 +1,18 @@
+from wsgiref.simple_server import make_server
+from src.controller.controller import TestController
+from src.config.router import MyRouter
+import os
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    yield 'Hello world!\n'
+
+def main():
+    httpd = make_server('', 9999, MyRouter())
+    print 'serving on port 9999'
+    
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    main()     
