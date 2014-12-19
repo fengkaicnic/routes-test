dde3ec6d4560f14fb52c337db2b8abc06b647835
fengkaicnic <fengkai@cnic.cn>
2014-12-19 14:07:24 +0800

    add a serve

---
+++ routes-test/src/service/resserver.py
@@ -0,0 +1,18 @@
+from wsgiref.simple_server import make_server
+from src.controller.controller import TestAction
+from src.config.router import MyResourceRouter
+import os
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    yield 'Hello world!\n'
+
+def main():
+    httpd = make_server('', 9997, MyResourceRouter())
+    print 'serving on port 9999'
+    
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    main()     
