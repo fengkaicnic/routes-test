3c50196ec06d6d3014b0f1cac5c9869ae1be5285
fengkaicnic <fengkai@cnic.cn>
2015-02-12 11:05:48 +0800

    change dec

--- routes-test/src/decorator/testdoc2.py
+++ routes-test/src/decorator/testdoc2.py
@@ -1,18 +1,22 @@
 class decorator(object):
-    def __init__(self, fn):
+    
+    abc = 35
+    def __init__(self, func=None, abc=None):
         print 'decorator'
-        self.fn = fn
-        print self.fn
+        self.abc = abc
+        self.fn = func
+#        print fn1
+        print self.abc
         
-    def __call__(self, fn):
-        print fn
+    def __call__(self, *args):
+        print self.fn
         def serve(*args):
             print args
-            return fn(*args)
+            return self.fn(*args)
         print '__call'
         return serve
 
-@decorator(123)
+@decorator(abc=123)
 def quare(a, b):
     print a**2
     return 'have lhxctfws'
